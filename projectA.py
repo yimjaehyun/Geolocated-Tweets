296798bfa01971ae68ab64d7af592034fd3eba26
yimjaehyun <jaehyunyim1997@gmail.com>
2019-04-30 10:02:11 -0700

    added title parsing

--- projectA.py
+++ projectA.py
@@ -3,10 +3,14 @@ import json
 import os
 import sys
 from pathlib import Path
+from scrapy.crawler import CrawlerProcess
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 
 TOTAL_SIZE = 5000   # in MB (5 GB)
 SIZE_OF_FILES = 10  # in MB (10 MB)
+links = []
 
 auth = tweepy.OAuthHandler(os.environ['API_KEY'], os.environ['API_SECRET'])
 #
@@ -38,15 +42,29 @@ class MyStreamListener(tweepy.StreamListener):
     arr = []
 
     def on_status(self, status):
+        global links
         print(status.text)
+        if status.place.full_name:
+            print(status.place.full_name)
 
-        json_status = json.loads(json.dumps(status._json))
-        data = {}
-        data['text'] = json_status['text']
-        data['created_at'] = json_status['created_at']
-        data['place'] = json_status['place']
-        data['user'] = json_status['user']
-        data['links'] = json_status['entities']
+        json_status = (status._json)
+        # data = {}
+        # data['text'] = json_status['text']
+        # data['created_at'] = json_status['created_at']
+        # data['place'] = json_status['place']['full_name']
+        # data['user'] = json_status['user']['screen_name']
+        # links_length = len(json_status['entities']['urls'])
+        # data['links'] = []
+        # if links_length > 0:
+        #     for i in range(links_length):
+        #         data['links'].append(json_status['entities']['urls'][i]['expanded_url'])
+
+        for i in range(len(json_status['entities']['urls'])):
+            url = str(json_status['entities']['urls'][i]['expanded_url'])
+            soup = BeautifulSoup(urlopen(url), features="lxml")
+            json_status['entities']['urls'][i]['title'] = soup.title.string
+            print(url + "\t" + soup.title.string)
+            # links.append(str(json_status['entities']['urls'][i]['expanded_url']))
 
         file_name = 'tweet' + str(self.i) + '.json'
         file_path = Path('./tweets/' + file_name)
@@ -54,12 +72,12 @@ class MyStreamListener(tweepy.StreamListener):
         if self.i == self.num_files:
             return False
 
-        self.arr.append(data)
-        output = json.dumps(self.arr, indent = 4)
+        self.arr.append(json_status)
+        output = json.dumps(self.arr)
         print(sys.getsizeof(output) / 1024)
-        if (sys.getsizeof(output) / 1024) >= 10000:
+        if (sys.getsizeof(output) / 1024) >= 10000: # 10000
             with open('./tweets/' + file_name, 'w') as outfile:
-                json.dump(self.arr, outfile, indent=4)
+                json.dump(self.arr, outfile)
             self.arr = []
             self.i = self.i+1
 
@@ -74,4 +92,8 @@ myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
 
 #-33.9,-75.0,-9.7,85.0
 #-122.75,36.8,-121.75,37.8,124.2,33.04,130.83,43.01,128.58,23.61,149.92,45.75
-myStream.filter(locations=[-33.9,-75.0,-9.7,85.0], is_async=True)
+myStream.filter(locations=[-118.308216,34.019505,-118.189212,34.094909])
+#
+# for link in links:
+#     soup = BeautifulSoup(urlopen(link))
+#     print(soup.title.string)
